94ee5b83f05d6bc710ae57e6b37d0b0b71fb4bd8
Decagon <Decagon@users.noreply.github.com>
2016-12-31 11:50:46 -0400

    Use csv format for easier tabulation/graphing

--- can-you-hear-me.sh
+++ can-you-hear-me.sh
@@ -6,7 +6,7 @@
 audioCaptureDuration="30s"
 
 # Where to save the volume adjustments
-volumeLocation="$HOME/can-you-hear-me/volume.txt"
+volumeDB="$HOME/can-you-hear-me/volume.csv"
 
 # Which output device to choose (change if it doesn't work)
 outputDevice="auto"
@@ -16,6 +16,12 @@ outputDevice="auto"
 outputDevices="$(pacmd list-sinks | grep -e 'name:' -e 'index' -e 'Speakers' | grep 'name')"
 numberOfOutputDevices=$(echo "$outputDevices" | wc -l)
 
+# Check if volume database exists; if not create it with a csv header
+if [ ! -f "$volumeDB" ]; then
+	echo "Date,Avg Output" >> "$volumeDB"
+fi
+
+# try to find a suitable output device
 if [ "$outputDevice" == "auto" ]; then
 	if (( numberOfOutputDevices > 1 )); then
 		echo "Warning: You have the following output devices: $outputDevices . I'll select the first one by default, but you should change it in the configuration settings."
@@ -26,19 +32,30 @@ if [ "$outputDevice" == "auto" ]; then
   fi
 fi
 
-# put current date in file
-"date" >> "$volumeLocation"
-
-parec -d $(echo "$outputDevices" |                             # get the name of it
+# get the average output volume
+outputVolumeFormatted=$(parec -d $(echo "$outputDevices" |     # get the name of it
 cut -d '<' -f 2 | 
 cut -d '>' -f 1).monitor |                                     # format the name so it works with lame
 (timeout $audioCaptureDuration lame -r -V0 -) |                # record for X seconds to stdout
 ffmpeg -i pipe:0 -af "volumedetect" -f null /dev/null 2>&1 |   # pipe audio to ffmpeg, get mean volume in dB
 grep "mean_volume" | 
-rev | cut -d " " -f 1-2 | rev >> "$volumeLocation"             # extract dB from output; save to file
+rev | cut -d " " -f 1-2 | rev)             		       # extract dB from output; save to file
+
+# get the output volume, and round it so that bash can use it
+outputVolumeDb=$(echo "$outputVolumeFormatted" | cut -d " " -f 1 | awk '{print int($1+0.5)}')
+
+if ((outputVolumeDb < -65 )); then
+	echo "Warning: I didn't detect any sound. Make sure that your output device is correctly selected."
+fi
+
+date=$(date)
+
+# write results to file
+echo "$date,$outputVolumeFormatted" >> "$volumeDB"
 
 # Credits
 
 # http://unix.stackexchange.com/questions/89571/how-to-get-volume-level-from-the-command-line
 # http://superuser.com/questions/323119/how-can-i-normalize-audio-using-ffmpeg
 # http://askubuntu.com/questions/229352/how-to-record-output-to-speakers
+# http://stackoverflow.com/questions/2395284/round-a-divided-number-in-bash
